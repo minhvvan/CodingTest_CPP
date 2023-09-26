36cbe45c5db681482d75faecc4c27d5dee2164dc
Lee Minhwan <59609086+minhvvan@users.noreply.github.com>
2023-09-27 02:00:34 +0900

    [level 2] Title: 3 x n 타일링, Time: 0.37 ms, Memory: 3.91 MB -BaekjoonHub

--- 프로그래머스/lv2/12902. 3 x n 타일링/3 x n 타일링.cpp
+++ 프로그래머스/lv2/12902. 3 x n 타일링/3 x n 타일링.cpp
@@ -2,27 +2,56 @@
 #include <vector>
 #include <iostream>
 #include <cmath>
+#include <bitset>
 
 using namespace std;
 
+
 int solution(int n) {
     int answer = 0;
+    int div = 1'000'000'007;
     
-    //0: ⨅, 1: □
-    vector<long long> tile(n+1, 0);
-    vector<int> temp;
+    //0: l,r,u, 1: ⨅ 
+    vector<vector<long long>> tile(n+1, vector<long long>(8, 0));
     
-    tile[0] = 1;
-    tile[2] = 3;
-
-    for(int i = 4; i <= n; i++) {
-        tile[i] += (tile[i - 2] * tile[2]) % 1'000'000'007;
-        for(int j = 4; j <= i; j += 2) {
-            tile[i] += (tile[i - j] * 2) % 1'000'000'007;
+    //1줄은 001, 100, 111만 가능
+    tile[1][1] = 1;
+    tile[1][4] = 1;
+    tile[1][7] = 1;
+    
+    // 000 001 010 011 100 101 110 111
+    
+    // 001
+    
+    for(int i = 2; i <= n; i++)
+    {
+        for(int j = 0; j < 8; j++)
+        {
+            if(j == 0)
+            {
+                tile[i][j] += tile[i-1][1] % div;
+                tile[i][j] += tile[i-1][4] % div;
+            }
+            
+            if(j == 1 || j == 4)
+            {
+                tile[i][j] += tile[i-1][0] % div;
+            }
+            
+            tile[i][j] += tile[i-1][7^j] % div;
         }
     }
-    
-    answer = tile[n] % 1'000'000'007;
 
-    return answer;
+    
+//     for(int i = 1; i <= n; i++)
+//     {
+//         for(int j = 0; j < 8; j++)
+//         {
+//             cout << tile[i][j] << " ";
+//         }
+        
+//         cout << "\n";
+//     }
+    
+    return tile[n][0] % div;
 }
