07d21ad0ef5d666c844c54397b232d5be4f499e2
Lee Minhwan <59609086+minhvvan@users.noreply.github.com>
2024-03-21 15:11:38 +0900

    [level 3] Title: 주사위 고르기, Time: 187.43 ms, Memory: 4.13 MB -BaekjoonHub

---
+++ 프로그래머스/3/258709. 주사위 고르기/주사위 고르기.cpp
@@ -0,0 +1,111 @@
+#include <string>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+void Roll(int currentSum, vector<int>& rollSum, int depth, vector<int>& picks, vector<vector<int>>& dice)
+{
+    if(depth == picks.size())
+    {
+        rollSum.push_back(currentSum);
+        return;
+    }
+    
+    for(int i = 0; i < 6; i++)
+    {
+        currentSum += dice[picks[depth]][i];
+        Roll(currentSum, rollSum, depth+1, picks, dice);
+
+        currentSum -= dice[picks[depth]][i];
+    }
+}
+
+vector<int> solution(vector<vector<int>> dice) {
+    vector<int> answer;
+    
+    //완탐
+    //n개중 n/2개 고르기
+    vector<vector<int>> APicks;
+    vector<vector<int>> BPicks;
+    
+    vector<int> temp;
+    vector<vector<int>> roll;
+    
+    for(int i = 0; i < (1 << dice.size()); i++)
+    {
+        vector<int> AtempPick;
+        vector<int> BtempPick;
+        
+        for(int j = 0; j < dice.size(); j++)
+        {
+            if(i & (1 << j)) AtempPick.push_back(j);
+            else BtempPick.push_back(j);
+        }
+        
+        if(AtempPick.size() == BtempPick.size())
+        {
+            APicks.push_back(AtempPick);
+            BPicks.push_back(BtempPick);
+        }
+    }
+    
+    
+    int maxWin = 0;
+    vector<int> candidate;
+    
+    for(int i = 0; i < APicks.size(); i++)
+    {
+        vector<int> ASums;
+        Roll(0, ASums, 0, APicks[i], dice);
+        
+        vector<int> BSums;
+        Roll(0, BSums, 0, BPicks[i], dice);
+        
+        sort(ASums.begin(),ASums.end());
+        sort(BSums.begin(),BSums.end()); 
+        
+//         cout << "ASums: ";
+//         for(auto sum : ASums)
+//         {
+//             cout << sum << " ";
+//         }
+//         cout << "\n";
+        
+//         cout << "BSums: ";
+//         for(auto sum : BSums)
+//         {
+//             cout << sum << " ";
+//         }
+//         cout << "\n";
+        
+        
+//         cout << "\n";
+        
+        int winRate1 = 0, winRate2 = 0; 
+        for(int n : ASums){
+            int win = lower_bound(BSums.begin(), BSums.end(), n) - BSums.begin(); 
+            if(win - 1 > 0) winRate1 += win; 
+        }
+        for(int n : BSums){
+            int win = lower_bound(ASums.begin(), ASums.end(), n) - ASums.begin(); 
+            if(win - 1 > 0) winRate2 += win; 
+        }
+        
+        if(winRate1 > maxWin)
+        {
+            maxWin = winRate1;
+            candidate = APicks[i];
+        }
+    }
+    
+    
+    
+    for(int i = 0; i < candidate.size(); i++)
+    {
+        answer.push_back(candidate[i]+1);
+    }
+    
+    return answer;
+}
