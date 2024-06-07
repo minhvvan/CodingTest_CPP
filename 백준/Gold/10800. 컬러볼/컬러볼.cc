6c251f8a3a0603285af4e217148b8728e4602218
Lee Minhwan <59609086+minhvvan@users.noreply.github.com>
2024-06-07 13:48:41 +0900

    [Gold II] Title: 컬러볼, Time: 88 ms, Memory: 9112 KB -BaekjoonHub

--- 백준/Gold/10800. 컬러볼/컬러볼.cc
+++ 백준/Gold/10800. 컬러볼/컬러볼.cc
@@ -1,52 +1,73 @@
-#include <iostream>
+#include <stdio.h>
+#include <string>
 #include <vector>
 #include <algorithm>
-#define MAX 200020
+#include <iostream>
+#include <sstream>
+#include <cmath>
+#include <climits>
+#include <queue>
+#include <map>
+#include <unordered_map>
+#include <set>
 
 using namespace std;
 
-struct ball{
-    int weight, color, idx;
+const int MAX = 200'020;
+
+int N;
+
+struct ball {
+    int size, color, idx;
 };
 
-int ans[MAX]={0,};
-int C[MAX]={0,};
-int S[MAX]={0,};
-int N;
-bool comp(ball &a, ball &b) {
-
-    if (a.weight==b.weight) return a.color < b.color;
-
-    return a.weight < b.weight;
+bool cmp(ball& a, ball& b) {
+    if (a.size == b.size) return a.color < b.color;
+    return a.size < b.size;
 }
-int main(void)
+
+int main()
 {
-    vector <ball> v;
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
 
     cin >> N;
-    for(int i=0;i<N;i++){
-        int weight,color;
-        cin >> color >> weight;
-        v.push_back({weight,color,i});
+    vector<ball> Balls;
+    vector<int> Colors(MAX, 0);
+    vector<int> Sizes(MAX, 0);
+    vector<int> Answer(MAX, 0);
+
+    for (int i = 0; i < N; i++)
+    {
+        int size, color;
+        cin >> color >> size;
+        Balls.push_back({ size,color,i });
     }
-    sort(v.begin(),v.end(),comp);
-    
-    int sum=0;
-    
-    for(int i=0;i<N;i++){
-        
-        int weight = v[i].weight;
-        int color = v[i].color;
-        int idx = v[i].idx;
-        
-        C[color]+=weight;
-        S[weight]+=weight;
-        sum+=weight;
-        
-        ans[idx]= sum - C[color] - S[weight] +weight;
-        if(i!=0 && (v[i-1].color == color) && (v[i-1].weight == weight))    ans[idx] = ans[v[i-1].idx];
+
+    sort(Balls.begin(), Balls.end(), cmp);
+
+    int sum = 0;
+
+    for (int i = 0; i < N; i++) 
+    {
+        int size = Balls[i].size;
+        int color = Balls[i].color;
+        int idx = Balls[i].idx;
+
+        Answer[idx] = sum - Colors[color] - Sizes[size];
+
+        Colors[color] += size;
+        Sizes[size] += size;
+        sum += size;
+
+        if (i != 0 && (Balls[i - 1].color == color) && (Balls[i - 1].size == size)) Answer[idx] = Answer[Balls[i - 1].idx];
     }
-    
-    for(int i=0;i<N;i++)    cout << ans[i] <<"\n";
+
+    for (int i = 0; i < N; i++)
+    {
+        cout << Answer[i] << "\n";
+    }
+
     return 0;
 }
