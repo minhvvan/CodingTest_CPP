a8c2b058d02dec352a9fc5d7465d6d996213379c
Lee Minhwan <59609086+minhvvan@users.noreply.github.com>
2024-07-02 13:30:31 +0900

    [Gold III] Title: K진 트리, Time: 44 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Gold/11812. K진 트리/K진 트리.cc
+++ 백준/Gold/11812. K진 트리/K진 트리.cc
@@ -1,35 +1,49 @@
-#include <iostream>
+#include <stdio.h>
+#include <cstring>
+#include <string>
+#include <vector>
 #include <algorithm>
+#include <iostream>
+#include <sstream>
 #include <cmath>
+#include <climits>
+#include <queue>
+#include <map>
+#include <unordered_map>
+#include <set>
+
 using namespace std;
 
-long long int n;
-int k, q;
+long long int N, K, Q;
 
-int main() {
-	ios::sync_with_stdio(false);
-	cin.tie(NULL);
-	cout.tie(NULL);
-	cin >> n >> k >> q;
-	long long int y, x;
-	for (int i = 0; i < q; ++i) {
-		cin >> y >> x;
-		long long int count = 0;
-		if (k == 1) {
-			count = abs(y - x);
-		}
-		else {
-			while (y != x) {
-				if (y > x) {
-					y = (y - 2) / k + 1;
-				}
-				else {
-					x = (x - 2) / k + 1;
-				}
-				++count;
-			}
-		}
-		cout << count << '\n';
-	}
-	return 0;
+int main()
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+
+    cin >> N >> K >> Q;
+
+    for (int i = 0; i < Q; ++i) {
+        long long int y, x;
+        cin >> y >> x;
+        long long int count = 0;
+        if (K == 1) {
+            count = abs(y - x);
+        }
+        else {
+            while (y != x) {
+                if (y > x) {
+                    y = (y - 2) / K + 1;
+                }
+                else {
+                    x = (x - 2) / K + 1;
+                }
+                ++count;
+            }
+        }
+        cout << count << '\n';
+    }
+
+    return 0;
 }
