c0b17f7e4ff85b7e36e0094e1a1fc5f4fd10c9f7
Lee Minhwan <59609086+minhvvan@users.noreply.github.com>
2024-08-16 10:47:18 +0900

    [Silver II] Title: N번째 큰 수, Time: 240 ms, Memory: 11008 KB -BaekjoonHub

--- 백준/Silver/2075. N번째 큰 수/N번째 큰 수.cc
+++ 백준/Silver/2075. N번째 큰 수/N번째 큰 수.cc
@@ -25,15 +25,32 @@ int main()
 
     cin >> N;
 
-    vector<int> Nums(N*N, 0);
-    for (int i = 0; i < N * N; i++)
+    vector<vector<int>> Nums(N, vector<int>(N, 0));
+    for (int i = 0; i < N; i++)
     {
-        cin >> Nums[i];
+        for (int j = 0; j < N; j++)
+        {
+            cin >> Nums[i][j];
+        }
     }
 
-    sort(Nums.begin(), Nums.end(), greater());
+    priority_queue < tuple<int, int, int>, vector<tuple<int, int, int>>, less<tuple<int, int, int>>> pq;
+    for (int i = 0; i < N; i++)
+    {
+        pq.push({ Nums[N - 1][i], N - 1, i });
+    }
 
-    cout << Nums[N - 1];
+    for (int i = 0; i < N-1; i++)
+    {
+        auto [num, y, x] = pq.top();
+        pq.pop();
+        if (y == 0) continue;
+
+        pq.push({ Nums[y - 1][x], y - 1, x });
+    }
+
+    auto [ans, y, x] = pq.top();
+    cout << ans;
 
     return 0;
 }
