61d2b186420f0b2043293860531818394628bd53
Lee Minhwan <59609086+minhvvan@users.noreply.github.com>
2024-08-17 12:52:19 +0900

    [Silver I] Title: 겹치는 건 싫어, Time: 128 ms, Memory: 7184 KB -BaekjoonHub

---
+++ 백준/Silver/20922. 겹치는 건 싫어/겹치는 건 싫어.cc
@@ -0,0 +1,63 @@
+#include <stdio.h>
+#include <cstring>
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <iostream>
+#include <sstream>
+#include <cmath>
+#include <climits>
+#include <queue>
+#include <map>
+#include <unordered_map>
+#include <set>
+#include <list>
+#include <bitset>
+
+using namespace std;
+
+int N, K;
+
+int main()
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+
+    cin >> N >> K;
+
+    deque<int> dq;
+    map<int, int> window;
+    int ans = 0;
+
+    for (int i = 0; i < N; i++)
+    {
+        int num;
+        cin >> num;
+
+        if (window[num] < K)
+        {
+            dq.push_back(num);
+            window[num]++;
+            ans = max(ans, (int)dq.size());
+            continue;
+        }
+
+        while (!dq.empty() && window[num] >= K)
+        {
+            int front = dq.front();
+            dq.pop_front();
+
+            window[front]--;
+        }
+
+        dq.push_back(num);
+        window[num]++;
+
+        ans = max(ans, (int)dq.size());
+    }
+
+    cout << ans;
+
+    return 0;
+}
