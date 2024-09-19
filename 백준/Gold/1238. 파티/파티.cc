7467b41756ad7be1859e5119f2ac9a9f67d80a74
Lee Minhwan <59609086+minhvvan@users.noreply.github.com>
2024-09-19 11:49:46 +0900

    [Gold III] Title: 파티, Time: 0 ms, Memory: 2292 KB -BaekjoonHub

--- 백준/Gold/1238. 파티/파티.cc
+++ 백준/Gold/1238. 파티/파티.cc
@@ -1,64 +1,90 @@
-#include <stdio.h>
-#include <cstring>
-#include <string>
-#include <vector>
 #include <algorithm>
 #include <iostream>
-#include <sstream>
-#include <cmath>
-#include <climits>
+#include <limits>
 #include <queue>
-#include <map>
-#include <unordered_map>
-#include <unordered_set>
-#include <set>
-#include <list>
-#include <bitset>
+#include <utility>
+#include <vector>
 
-using namespace std;
+class Graph {
+public:
+  Graph(int, int);
+  int solution(int);
 
-int N, M, X;
-vector<vector<pair<int, int>>> roads;
-vector<vector<int>> result;
-const int MAX = 1000 * 100 + 1;
+private:
+  using Edge = std::pair<int, int>;
 
-int main()
-{
-    ios_base::sync_with_stdio(0);
-    cin.tie(0);
-    cout.tie(0);
+  std::vector<std::vector<Edge>> graph_;
+  std::vector<std::vector<Edge>> reverse_graph_;
+  int num_vertices_;
 
-    cin >> N >> M >> X;
+  void compute_minimum_distance_(const std::vector<std::vector<Edge>> &,
+                                 std::vector<int> &, int);
+};
 
-    roads.resize(N + 1);
-    result.resize(N + 1, vector<int>(N + 1, MAX));
-    for (int i = 0; i < M; i++)
-    {
-        int a, b, c;
-        cin >> a >> b >> c;
-        roads[a].push_back({ b, c });
-        result[a][b] = c;
+int main(int argc, const char **argv) {
+  std::ios_base::sync_with_stdio(false);
+  std::cin.tie(nullptr);
+
+  int num_vertices = 0, num_edges = 0, target = 0;
+  std::cin >> num_vertices >> num_edges >> target;
+
+  Graph graph = Graph(num_vertices, num_edges);
+  std::cout << graph.solution(target) << '\n';
+
+  return 0;
+}
+
+Graph::Graph(int num_vertices, int num_edges)
+    : graph_(std::vector<std::vector<Edge>>(num_vertices + 1)),
+      reverse_graph_(std::vector<std::vector<Edge>>(num_vertices + 1)),
+      num_vertices_(num_vertices) {
+  int u = 0, v = 0, weight = 0;
+  while (num_edges--) {
+    std::cin >> u >> v >> weight;
+    graph_[u].push_back({v, weight});
+    reverse_graph_[v].push_back({u, weight});
+  }
+}
+
+int Graph::solution(int target) {
+  std::vector<int> final_distance(num_vertices_ + 1, 0);
+  compute_minimum_distance_(graph_, final_distance, target);
+  compute_minimum_distance_(reverse_graph_, final_distance, target);
+
+  return *std::max_element(final_distance.begin(), final_distance.end());
+}
+
+void Graph::compute_minimum_distance_(
+    const std::vector<std::vector<Edge>> &graph,
+    std::vector<int> &final_distance, int target) {
+  int inf = std::numeric_limits<int>::max();
+  std::vector<int> distance(num_vertices_ + 1, inf);
+
+  auto cmp = [](const Edge &e1, const Edge &e2) {
+    return e1.second > e2.second;
+  };
+  std::priority_queue<Edge, std::vector<Edge>, decltype(cmp)> pq(cmp);
+
+  pq.push({target, 0});
+  distance[target] = 0;
+
+  while (!pq.empty()) {
+    auto [current_vertex, current_distance] = pq.top();
+    pq.pop();
+
+    if (distance[current_vertex] < current_distance)
+      continue;
+
+    for (auto [adjacent_vertex, weight] : graph[current_vertex]) {
+      int new_distance = current_distance + weight;
+      if (new_distance > distance[adjacent_vertex])
+        continue;
+
+      pq.push({adjacent_vertex, new_distance});
+      distance[adjacent_vertex] = new_distance;
     }
+  }
 
-    for (int k = 1; k <= N; k++)
-    {
-        for (int i = 1; i <= N; i++)
-        {
-            for (int j = 1; j <= N; j++)
-            {
-                result[i][j] = min(result[i][j], result[i][k] + result[k][j]);
-            }
-        }
-    }
-
-    int ans = 0;
-    for (int i = 1; i <= N; i++)
-    {
-        if (i == X) continue;
-        ans = max(ans, result[i][X] + result[X][i]);
-    }
-
-    cout << ans;
-
-    return 0;
+  for (int i = 1; i <= num_vertices_; i++)
+    final_distance[i] += distance[i];
 }
