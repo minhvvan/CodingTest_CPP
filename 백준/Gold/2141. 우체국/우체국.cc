e893f0a3c022e6255c3c81b2caaf230f9ae33661
Lee Minhwan <59609086+minhvvan@users.noreply.github.com>
2024-10-06 10:30:05 +0900

    [Gold IV] Title: 우체국, Time: 32 ms, Memory: 3688 KB -BaekjoonHub

--- 백준/Gold/2141. 우체국/우체국.cc
+++ 백준/Gold/2141. 우체국/우체국.cc
@@ -1,25 +1,57 @@
-#include <cstdio>
+#include <stdio.h>
+#include <cstring>
+#include <string>
+#include <vector>
 #include <algorithm>
+#include <iostream>
+#include <sstream>
+#include <cmath>
+#include <climits>
+#include <queue>
+#include <map>
+#include <unordered_map>
+#include <unordered_set>
+#include <set>
+#include <list>
+#include <bitset>
+
 using namespace std;
-using ll = long long;
+
 int N;
-pair<ll,ll> xa[100001];
- 
-int main(){
-    scanf("%d",&N);
-    ll sum=0;
-    for(int i=1;i<=N;i++) {
-        scanf("%lld%lld",&xa[i].first,&xa[i].second);
-        sum+=xa[i].second;
+vector<pair<int,int>> people;
+
+int main()
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    cout.precision(4);
+
+    cin >> N;
+
+    long long total = 0;
+    for (int i = 0; i < N; i++)
+    {
+        int x, a;
+        cin >> x >> a;
+
+        total += a;
+        people.push_back({ x, a });
     }
-    sort(xa+1,xa+N+1);
- 
-    ll cur=0;
-    for(int i=1;i<=N;i++){
-        cur+=xa[i].second;
-        if(cur>=(sum+1)/2) {
-            printf("%d",xa[i].first);
+
+    sort(people.begin(), people.end());
+
+    long long current = 0;
+    for (int i = 0 ; i < N; i++)
+    {
+        current += people[i].second;
+
+        if (current >= (total + 1) / 2)
+        {
+            cout << people[i].first;
             break;
         }
     }
+
+    return 0;
 }
