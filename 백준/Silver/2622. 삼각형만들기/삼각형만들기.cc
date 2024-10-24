d92af8824eff94ebf8c33d3b23ebd074720104b1
Lee Minhwan <59609086+minhvvan@users.noreply.github.com>
2024-10-24 23:03:15 +0900

    [Silver V] Title: 삼각형만들기, Time: 220 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/2622. 삼각형만들기/삼각형만들기.cc
@@ -0,0 +1,37 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int N;
+
+int main() {
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    cout.precision(4);
+
+    cin >> N;
+
+    int ans = 0;
+
+    for (int i = 1; i <= N / 3; i++)
+    {
+        int rest = N - i;
+
+        for (int j = i; j <= rest / 2; j++)
+        {
+            int a = min({ i, j, rest - j });
+            int c = max({ i, j, rest - j });
+            int b = N - a - c;
+
+            if (c < N - c)
+            {
+                ans++;
+            }
+        }
+    }
+
+    cout << ans;
+
+    return 0;
+}
