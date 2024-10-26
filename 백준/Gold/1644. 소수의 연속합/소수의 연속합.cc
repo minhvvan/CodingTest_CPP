3cddc6f20ae74e62da8fbd7f316a08bfcf11b04c
Lee Minhwan <59609086+minhvvan@users.noreply.github.com>
2024-10-26 13:22:58 +0900

    [Gold III] Title: 소수의 연속합, Time: 1572 ms, Memory: 7416 KB -BaekjoonHub

---
+++ 백준/Gold/1644. 소수의 연속합/소수의 연속합.cc
@@ -0,0 +1,58 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int N;
+vector<int> prime = {0};
+vector<int> sumStack = {0};
+
+int main() {
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    cout << fixed;
+    cout.precision(1);
+    
+    cin >> N;
+
+    long long ans = 0;
+
+    for (int i = 2; i <= N; i++)
+    {
+        bool bPrime = true;
+        for (int j = 2; j <= sqrt(i); j++)
+        {
+            if (i % j == 0)
+            {
+                bPrime = false;
+                break;
+            }
+        }
+
+        if (bPrime)
+        {
+            prime.push_back(i);
+            sumStack.push_back(sumStack.back() + i);
+        }
+    }
+
+    int left = 1;
+    int right = 1;
+
+    while (left < prime.size() && right < prime.size())
+    {
+        int sum = sumStack[right] - sumStack[left - 1];
+
+        if (sum == N)
+        {
+            ans++;
+            right++;
+        }
+        else if (sum > N) left++;
+        else right++;
+    }
+
+    cout << ans;
+
+    return 0;
+}
