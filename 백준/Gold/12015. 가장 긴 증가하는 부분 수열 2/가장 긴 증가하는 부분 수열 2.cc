c8f673653f78f4f1f99e116b2c75efaad3f136fe
Lee Minhwan <59609086+minhvvan@users.noreply.github.com>
2024-11-15 23:16:44 +0900

    [Gold II] Title: 가장 긴 증가하는 부분 수열 2, Time: 144 ms, Memory: 9836 KB -BaekjoonHub

--- 백준/Gold/12015. 가장 긴 증가하는 부분 수열 2/가장 긴 증가하는 부분 수열 2.cc
+++ 백준/Gold/12015. 가장 긴 증가하는 부분 수열 2/가장 긴 증가하는 부분 수열 2.cc
@@ -15,22 +15,18 @@ int main() {
 
     vector<int> nums(N);
     vector<int> last(N);
-    vector<int> dp(N, 0);
     for (int i = 0; i < N; i++)
     {
         cin >> nums[i];
     }
 
-    int len = 1;
-    last[0] = nums[0];
-    dp[0] = 1;
-    for (int i = 1; i < N; i++)
+    int len = 0;
+    for (int i = 0; i < N; i++)
     {
         int pos = lower_bound(last.begin(), last.begin() + len, nums[i]) - last.begin();
-        dp[i] = pos+1;
 
         last[pos] = nums[i];
-        if (dp[i] > len) len++;
+        if (pos + 1 > len) len++;
     }
 
     cout << len;
