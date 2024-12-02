7f6bbc5420fde3c873a31fe5c89b25bfa26a3eb3
Lee Minhwan <59609086+minhvvan@users.noreply.github.com>
2024-12-02 13:30:48 +0900

    [Gold IV] Title: 미로만들기, Time: 0 ms, Memory: 2160 KB -BaekjoonHub

---
+++ 백준/Gold/2665. 미로만들기/미로만들기.cc
@@ -0,0 +1,69 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int N, ans = INT_MAX;
+vector<int> dy = { -1, 0, 1, 0 };
+vector<int> dx = { 0, 1, 0, -1 };
+
+void bfs(vector<vector<int>>& rooms)
+{
+    deque<tuple<int, int, int>> dq;
+    vector<vector<int>> visited(N, vector<int>(N, INT_MAX));
+    dq.push_back({ 0,0,0 });
+
+    while (!dq.empty())
+    {
+        auto [cost, y, x] = dq.front();
+        dq.pop_front();
+
+        if (visited[y][x] <= cost) continue;
+        if (ans <= cost) continue;
+        visited[y][x] = cost;
+
+        for (int i = 0; i < 4; i++)
+        {
+            int newY = y + dy[i];
+            int newX = x + dx[i];
+
+            if (newY < 0 || newY >= N || newX < 0 || newX >= N) continue;
+            if (newY == N - 1 && newX == N - 1)
+            {
+                ans = min(ans, cost);
+                continue;
+            }
+            if (visited[newY][newX] <= cost) continue;
+
+            if (rooms[newY][newX] == 0) dq.push_back({ cost + 1, newY, newX });
+            else dq.push_front({ cost, newY, newX });
+        }
+    }
+}
+
+int main() {
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    cout << fixed;
+    cout.precision(4);
+
+    cin >> N;
+
+    //0: 검은방 1: 흰방
+    vector<vector<int>> rooms(N, vector<int>(N));
+    for (int i = 0; i < N; i++)
+    {
+        string str;
+        cin >> str;
+
+        for (int j = 0; j < N; j++)
+        {
+            rooms[i][j] = str[j]-'0';
+        }
+    }
+
+    bfs(rooms);
+    cout << ans;
+
+    return 0;
+}
