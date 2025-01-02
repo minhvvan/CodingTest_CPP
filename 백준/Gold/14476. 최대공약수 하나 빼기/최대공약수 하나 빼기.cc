b363411281083699479d7a375bcc715e12a486b6
Lee Minhwan <59609086+minhvvan@users.noreply.github.com>
2025-01-02 17:26:59 +0900

    [Gold II] Title: 최대공약수 하나 빼기, Time: 152 ms, Memory: 13744 KB -BaekjoonHub

--- 백준/Gold/14476. 최대공약수 하나 빼기/최대공약수 하나 빼기.cc
+++ 백준/Gold/14476. 최대공약수 하나 빼기/최대공약수 하나 빼기.cc
@@ -2,67 +2,65 @@
 
 using namespace std;
 
-int gcd(int a, int b) {
-	return b ? gcd(b, a % b) : a;
+int N;
+vector<int> nums;
+
+int gcd(int a, int b) 
+{
+    return b ? gcd(b, a % b) : a;
 }
 
 int main() {
-	int n;
-	scanf("%d", &n);
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    cout << fixed;
 
-	vector<int> num;
-	for (int i = 0; i < n; i++) {
-		int x;
-		scanf("%d", &x);
-		num.push_back(x);
+    cin >> N;
+    nums.resize(N);
+
+    for (int i = 0; i < N; i++)
+    {
+        cin >> nums[i];
+    }
+
+	vector<int> LtoR(N);
+	vector<int> RtoL(N);
+
+	LtoR[0] = nums[0];
+	RtoL[N - 1] = nums.back();
+
+	for (int i = 1; i <= N - 1; i++)
+	{
+		LtoR[i] = gcd(LtoR[i - 1], nums[i]);
 	}
 
-	vector<int> LtoR(n);
-	vector<int> RtoL(n);
-
-	LtoR[0] = num[0];
-	RtoL[n - 1] = num.back();
-
-	//LtoR
-	for (int i = 1; i <= n - 1; i++) {
-		int val = LtoR[i - 1];
-		LtoR[i] = gcd(max(val, num[i]), min(val, num[i]));
+	for (int i = N - 2; i >= 0; i--) 
+	{
+		RtoL[i] = gcd(RtoL[i + 1], nums[i]);
 	}
 
-	// RtoL
-	for (int i = n - 2; i >= 0; i--) {
-		int val = RtoL[i + 1];
-		RtoL[i] = gcd(max(val, num[i]), min(val, num[i]));
-	}
+	int result = RtoL[1] < LtoR[N - 2] ? LtoR[N - 2] : RtoL[1];
+	int except = RtoL[1] < LtoR[N - 2] ? nums[N - 1] : nums[0];
 
-	int result = -1;
-	int except = -1;
-
-	for (int i = 0; i < n; i++) {
-		if (i == 0) {
-			result = max(result, RtoL[1]);
-			except = num[i];
-		}
-		else if (i == n - 1) {
-			if (result < LtoR[n-2]) {
-				result = LtoR[n-2];
-				except = num[i];
-			}
-		}
-		else {
-			if (result < gcd(LtoR[i - 1], RtoL[i + 1])) {
-				result = gcd(LtoR[i - 1], RtoL[i + 1]);
-				except = num[i];
-			}
+	for (int i = 1; i < N - 1; i++)
+	{
+		int current = gcd(LtoR[i - 1], RtoL[i + 1]);
+		if (result < current)
+		{
+			result = current;
+			except = nums[i];
 		}
 	}
 
-	if (except % result == 0) {
-		printf("-1");
+	if (except % result == 0) 
+	{
+		cout << "-1";
 	}
-	else {
-		printf("%d %d", result, except);
+	else 
+	{
+		cout << result << " " << except;
 	}
-
-	return 0;
+    
+    return 0;
 }
