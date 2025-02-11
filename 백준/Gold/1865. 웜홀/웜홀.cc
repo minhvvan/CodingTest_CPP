03231ecd7d614dcbbc561e4b2a0bdb3899fb0adf
Lee Minhwan <59609086+minhvvan@users.noreply.github.com>
2025-02-11 13:16:15 +0900

    [Gold III] Title: 웜홀, Time: 20 ms, Memory: 2332 KB -BaekjoonHub

--- 백준/Gold/1865. 웜홀/웜홀.cc
+++ 백준/Gold/1865. 웜홀/웜홀.cc
@@ -10,6 +10,44 @@ struct Edge
 	int dist;
 };
 
+vector<Edge> edges;
+
+void bellman()
+{
+	vector<int> dist(N + 1, 987654321);
+	dist[1] = 0;
+
+	for (int i = 1; i < N; i++)
+	{
+		for (auto edge : edges)
+		{
+			int from = edge.start;
+			int to = edge.end;
+			int cost = edge.dist;
+
+			if (dist[to] > dist[from] + cost)
+			{
+				dist[to] = dist[from] + cost;
+			}
+		}
+	}
+
+	for (auto edge : edges)
+	{
+		int from = edge.start;
+		int to = edge.end;
+		int cost = edge.dist;
+
+		if (dist[to] > dist[from] + cost)
+		{
+			cout << "YES" << "\n";
+			return;
+		}
+	}
+
+	cout << "NO" << "\n";
+}
+
 int main()
 {
 	ios_base::sync_with_stdio(0);
@@ -21,8 +59,8 @@ int main()
 
 	while (T--)
 	{
-		vector<Edge> edges;
 		cin >> N >> M >> W;
+		edges.clear();
 
 		for (int i = 0; i < M; i++)
 		{
@@ -41,49 +79,8 @@ int main()
 			edges.push_back({ a,b,-c });
 		}
 
-		bool bflag = false;
-		vector<bool> visited(N + 1, false);
-		for (int k = 1; k <= N; k++)
-		{
-			vector<int> dist(N+1, INT_MAX);
-			dist[k] = 0;
-
-			if (visited[k]) continue;
-			visited[k] = true;
-
-			for (int i = 0; i < N; i++)
-			{
-				for (auto edge : edges)
-				{
-					int from = edge.start;
-					int to = edge.end;
-					int cost = edge.dist;
-
-					if (dist[from] == INT_MAX) continue;
-					visited[from] = true;
-
-					if (dist[to] > dist[from] + cost)
-					{
-						dist[to] = dist[from] + cost;
-						if (i == N - 1)
-						{
-							bflag = true;
-							break;
-						}
-					}
-				}
-			}
-
-			if (bflag)
-			{
-				cout << "YES\n";
-				break;
-			}
-		}
-
-		if (!bflag) cout << "NO\n";
+		bellman();
 	}
 	
-
 	return 0;
 }
