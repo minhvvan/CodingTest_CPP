904542a1adc2ffd240862c1a5963ff9191322def
Lee Minhwan <59609086+minhvvan@users.noreply.github.com>
2025-02-19 10:39:49 +0900

    [Gold III] Title: 오등큰수, Time: 216 ms, Memory: 13748 KB -BaekjoonHub

--- 백준/Gold/17299. 오등큰수/오등큰수.cc
+++ 백준/Gold/17299. 오등큰수/오등큰수.cc
@@ -1,43 +1,58 @@
-#include <iostream>
-#include <vector>
-#include <stack>
+#include <bits/stdc++.h>
+
 using namespace std;
-#define MAX 1000000
+
+int N;
+vector<int> nums;
+vector<int> numCnt(1'000'001, 0);
+vector<int> dp;
+
+int GetRoot(int idx, int cnt)
+{
+	if (dp[idx] == -1 || cnt < numCnt[nums[idx]]) return idx;
+	return GetRoot(dp[idx], cnt);
+}
 
 int main()
 {
-	cin.tie(0);
 	ios_base::sync_with_stdio(0);
+	cin.tie(0);
+	cout.tie(0);
+	cout << fixed;
 
-	int n;
-	cin >> n;
+	cin >> N;
+	nums.resize(N);
+	dp.resize(N, -1);
 
-	vector<int> arr(n + 1);
-	vector<int> ngf(n + 1, - 1);
-	vector<int> cnt(MAX + 1, 0);
-	stack<int> st;
-
-	for (int i = 1; i <= n; i++)
+	for (int i = 0; i < N; i++)
 	{
-		cin >> arr[i];
-		cnt[arr[i]]++;
+		cin >> nums[i];
+		numCnt[nums[i]]++;
 	}
 
-	for (int i = 1; i <= n; i++)
+	for (int i = N - 2; i >= 0; i--)
 	{
-		while (!st.empty() && cnt[arr[st.top()]]< cnt[arr[i]])
+		//오른쪽 수가 현재 수보다 많이 등장했을 때
+		if (numCnt[nums[i]] < numCnt[nums[i + 1]])
 		{
-			ngf[st.top()] = arr[i];
-			st.pop();
+			dp[i] = i + 1;
 		}
-		st.push(i);
+		else if (numCnt[nums[i]] == numCnt[nums[i + 1]])
+		{
+			dp[i] = dp[i + 1];
+		}
+		else
+		{
+			int temp = GetRoot(i + 1, numCnt[nums[i]]);
+			if (numCnt[nums[i]] < numCnt[nums[temp]]) dp[i] = temp;
+		}
 	}
 
-	for (int i = 1; i <= n; i++)
+	for (auto ans : dp)
 	{
-		cout << ngf[i] << ' ';
+		if (ans == -1) cout << -1 << " ";
+		else cout << nums[ans] << " ";
 	}
-	cout << '\n';
 
 	return 0;
 }
