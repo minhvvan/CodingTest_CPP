520c62a72d66d528838c394f2fc6ddad29c4adf3
Lee Minhwan <59609086+minhvvan@users.noreply.github.com>
2025-03-14 09:27:35 +0900

    [Silver III] Title: 병든 나이트, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/1783. 병든 나이트/병든 나이트.cc
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+	ios::sync_with_stdio(false);
+	cin.tie(0);
+	cout.tie(0);
+
+    int n, m;
+
+    cin >> n >> m;
+
+    if (n == 1) cout << 1;
+    else if (n == 2) cout << min(4, (m + 1) / 2);
+    else if (m < 7) cout << min(4, m);
+    else cout << m - 2;
+
+	return 0;
+}
