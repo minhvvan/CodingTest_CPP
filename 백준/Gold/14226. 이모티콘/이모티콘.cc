a5c238c2594527b3f32ed34e1431c55297256638
Lee Minhwan <59609086+minhvvan@users.noreply.github.com>
2025-04-07 14:12:35 +0900

    [Gold IV] Title: 이모티콘, Time: 176 ms, Memory: 94692 KB -BaekjoonHub

---
+++ 백준/Gold/14226. 이모티콘/이모티콘.cc
@@ -0,0 +1,55 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define INPUT_OPTIMIZE cin.tie(NULL); cout.tie(NULL); ios::sync_with_stdio(false);
+#define INF 2e9
+
+using namespace std;
+int N;
+const int MAX = 1002;
+int ans = INT_MAX;
+vector<int> visited(MAX, INT_MAX);
+
+int main() 
+{
+    INPUT_OPTIMIZE;
+
+    cin >> N;
+
+	vector<bool> visit(3001, false);
+
+	queue<tuple<int, int, int>> q;
+	q.push({ 1,0, 0 });
+
+	while (!q.empty()) 
+	{
+		auto [screen, clip, time] = q.front();
+		q.pop();
+
+		if (screen == N)
+		{
+			cout << time;
+			return 0;
+		}
+
+		//복사
+		if (!visit[screen])
+		{
+			visit[screen] = 1;
+			q.push({ screen, screen, time + 1 });
+		}
+
+		//붙여넣기
+		if (screen + clip <= 3000)
+		{
+			q.push({ screen + clip, clip, time + 1 });
+		}
+
+		//지우기
+		if (screen - 1 >= 0)
+		{
+			q.push({ screen - 1, clip, time + 1 });
+		}
+	}
+
+    return 0;
+}
