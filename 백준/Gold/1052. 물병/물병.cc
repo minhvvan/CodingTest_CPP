7e1e45e1c938af9bc0ab8c53f48cf1179925994f
Lee Minhwan <59609086+minhvvan@users.noreply.github.com>
2025-04-24 21:41:58 +0900

    [Gold V] Title: 물병, Time: 24 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Gold/1052. 물병/물병.cc
+++ 백준/Gold/1052. 물병/물병.cc
@@ -22,23 +22,8 @@ int main()
 
 		while (1) 
 		{
-			int cnt = 0;
-			int temp = n;
-			while (temp > 0) 
-			{
-				if (temp % 2 == 0) 
-				{
-					temp /= 2;
-				}
-				else 
-				{
-					temp /= 2;
-					cnt++;
-				}
-			}
-
-			//cnt가 k보다 작거나 같아지면 종료
-			if (k >= cnt) break;
+			bitset<100> num(n);
+			if (num.count() <= k) break;
 
 			n++;
 			answer++;
