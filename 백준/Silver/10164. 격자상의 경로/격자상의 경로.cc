8340303d47fdab66bfc11565ed49060cd1dab142
Lee Minhwan <59609086+minhvvan@users.noreply.github.com>
2025-05-03 14:26:54 +0900

    [Silver II] Title: 격자상의 경로, Time: 0 ms, Memory: 2024 KB, Score: 32 point -BaekjoonHub

---
+++ 백준/Silver/10164. 격자상의 경로/격자상의 경로.cc
@@ -0,0 +1,50 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define INPUT_OPTIMIZE cin.tie(NULL); cout.tie(NULL); ios::sync_with_stdio(false);
+#define INF 2e9
+
+using namespace std;
+int N, M, K;
+vector<vector<int>> dp;
+
+int main() 
+{
+    INPUT_OPTIMIZE;
+
+    cin >> N >> M >> K;
+
+    dp.resize(N + 1, vector<int>(M + 1, 0));
+    dp[1][1] = 1;
+
+    int y = K / M + 1;
+    int x = K % M;
+
+    if (K == 0)
+    {
+        y = N;
+        x = M;
+    }
+
+    for (int i = 1; i <= y; i++)
+    {
+        for (int j = 1; j <= x; j++)
+        {
+            dp[i][j] += dp[i - 1][j];
+            dp[i][j] += dp[i][j - 1];
+        }
+    }
+
+    for (int i = y; i <= N; i++)
+    {
+        for (int j = x; j <= M; j++)
+        {
+            if (i == y && j == x) continue;
+            dp[i][j] += dp[i - 1][j];
+            dp[i][j] += dp[i][j - 1];
+        }
+    }
+
+    cout << dp[N][M];
+
+    return 0;
+} 
